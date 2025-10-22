900e423eb4a85d611dd4b48d95116d61c1f34cf6
janchpanch <31480952+janchpanch@users.noreply.github.com>
2025-10-21 20:22:48 -0500

    Rebuilt with React + JS

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+export default function App() {
+
+  return (
+    <div>
+      <h1>Joe's Webdev Sandbox</h1>
+      <p> Hopefully this deployed properly! Will be updated regularly as I learn web development!</p>
+      <h2>TODO</h2>
+      <ol>
+        <li>Discreet component structure</li>
+        <li>Multipage/view functionality (react-router-dom or tailwindcss)</li>
+        <li>Changelog to document progress</li>
+      </ol>
+      <p>&#169; Joseph Pancho. All rights reserved.</p>
+    </div>
+  );
+}
+

a91e3130dec89ac3f9bf39ecd2517766766f8423
janchpanch <31480952+janchpanch@users.noreply.github.com>
2025-11-18 10:42:16 -0600

    Update Home.jsx with revised plan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ const Home = () => {
       <Hero />
       <h3 className="font-bold">TODO</h3>
       <ol>
-        <li>Create specialized components per page</li>
-        <li>Explore GitHub REST API usage to pull commits to display for changelog</li>
+        <li>Explore tools/platforms that automate styling full stop</li>
+        <li>Rebuild this website such that focus can be on backend development</li>
         
         
       </ol>
